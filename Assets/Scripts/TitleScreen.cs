6223cf477591473fcc1f02e134b3d58ed77414c6
jfriso <jayfriso@gmail.com>
2017-01-22 14:35:05 -0800

    Added delay

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -6,19 +6,26 @@ public class TitleScreen : MonoBehaviour {
     private AudioController audioController;
     public float startMidPitch;
     public float startLowCutoff;
-	
+    public float startDelay = 3f;
+    private bool startable = false;
     void Start() {
         audioController = GetComponentInChildren<AudioController>();
+        StartCoroutine(initiateDelay());
     }
 
 	// Update is called once per frame
 	void Update () {
         float pitch = audioController.getPitch();
 
-        if (pitch<startMidPitch && pitch > startLowCutoff) {
+        if (pitch<startMidPitch && pitch > startLowCutoff && startable) {
             GameManager.instance.firstStart();
         }
 	}
 
+    private IEnumerator initiateDelay() {
+        yield return new WaitForSeconds(startDelay);
+        startable = true;
+        yield return null;
+    }
     
 }
