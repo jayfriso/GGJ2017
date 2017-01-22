53d593e4f1d5936527ecffdb0e4bc97eea2993b1
Abdullah Abdulqader <UP2LLAA@GMAIL.COM>
2017-01-21 21:52:26 -0800

    ChasingSpirits refined

--- Assets/Scripts/ChasingMover.cs
+++ Assets/Scripts/ChasingMover.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class ChasingMover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed;
+	float playerYPos;
+	float initialYPos;
+	public float acceleration;
+	public float minYSpeed;
+	public float maxYSpeed;
+	public float minZRotation;
+	public float maxZRotation;
+
+	void Start ()
+	{
+       	initialYPos = transform.position.y;
+		GetComponent<Rigidbody2D> ().velocity = new Vector3 (-speed, GetComponent<Rigidbody2D> ().velocity.y - acceleration, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate ()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		playerYPos = player.transform.position.y;
+
+		if (playerYPos > transform.position.y) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector3 (-speed, GetComponent<Rigidbody2D> ().velocity.y + acceleration, 0);
+//			transform.position = new Vector3(transform.position.x, transform.position.y + acceleration, transform.position.z);
+		} else if (playerYPos < transform.position.y) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector3 (-speed, GetComponent<Rigidbody2D> ().velocity.y - acceleration, 0);
+//			transform.position = new Vector3(transform.position.x, transform.position.y - acceleration, transform.position.z);
+		}
 		
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -23,9 +23,13 @@ public class GameController : MonoBehaviour {
 		while (true) {
 			Vector2 spawnPosition = new Vector2 (spawnCoordinates.x, Random.Range (-spawnCoordinates.y, spawnCoordinates.y));
 			Vector2 spawnPosition2 = new Vector2 (spawnCoordinates.x, Random.Range (-spawnCoordinates.y, spawnCoordinates.y));
+			Vector2 spawnPosition3 = new Vector2 (spawnCoordinates.x, Random.Range (-spawnCoordinates.y, spawnCoordinates.y));
+
 			Instantiate (StraightSpirit, spawnPosition, Quaternion.identity);
 			yield return new WaitForSeconds (spawnWaitTime);
 			Instantiate (WavySpirit, spawnPosition2, Quaternion.identity);
+			yield return new WaitForSeconds (spawnWaitTime);
+			Instantiate (ChasingSpirit, spawnPosition3, Quaternion.identity);
 			yield return new WaitForSeconds (spawnWaitTime);
 		}
 	}
