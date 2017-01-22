24de3f39ddc4646e88ead122e555bec6e49eb0d0
jfriso <jayfriso@gmail.com>
2017-01-22 10:34:15 -0800

    Implemented the highscore system.

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -8,9 +8,15 @@ public class GameUI : MonoBehaviour {
     public GameObject gameOverScreen;
     private string scoreString = "SCORE: ";
 
+    public GameObject newHighScoreUI;
+    public GameObject scoreTextUI;
+    private string newHighScoreText = "New High Score!!! \n \n ";
+    private string scoreText1 = "Current Score: ";
+    private string scoreText2 = "\n \n High Score: ";
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         setScoreText(0);
 	}
 	
@@ -20,5 +26,17 @@ public class GameUI : MonoBehaviour {
 
     public void showGameOver(bool show) {
         gameOverScreen.SetActive(show);
+    }
+
+    public void showScoreScreen(bool isHighScore, int score, int highScore) {
+        if (isHighScore) {
+            scoreTextUI.SetActive(false);
+            newHighScoreUI.SetActive(true);
+            newHighScoreUI.GetComponent<Text>().text = newHighScoreText + score; 
+        } else {
+            scoreTextUI.SetActive(true);
+            newHighScoreUI.SetActive(false);
+            scoreTextUI.GetComponent<Text>().text = scoreText1 + score + scoreText2 + highScore;
+        }
     }
 }
